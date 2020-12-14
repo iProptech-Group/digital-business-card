b26a91415523828b36d8b19c7c0d3d598e650e83
Hung Pham <phamphihungbk@gmail.com>
2020-12-14 22:08:04 +0800

    initialize project

---
+++ web/pages/contact-us.vue
@@ -0,0 +1,15 @@
+<template>
+  <TheMenu />
+</template>
+
+<script>
+    import TheMenu from "../components/partials/TheMenu";
+    export default {
+        name: 'ContactUs',
+        components: {TheMenu}
+    };
+</script>
+
+<style scoped>
+
+</style>
